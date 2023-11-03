7cd55f45629ecf58371c771ea581bcf6e99e1ef0
garipovsamat <s.garipov@diginavis.com>
2023-11-03 15:10:25 +0300

    first result

--- src/components/Asteroid/Asteroid.tsx
+++ src/components/Asteroid/Asteroid.tsx
@@ -1,10 +1,9 @@
 import { FunctionComponent, useCallback, useMemo, useState } from "react";
 import Asteroid__ from "./Asteroid.module.css";
 import { INearEarthObject } from "@/app/api/neo/neo_client";
-import cn from 'classnames';
 
-export const Asteroid:FunctionComponent<{className?: string, object: INearEarthObject, isLunar?: boolean, isOrdered?: boolean, addOrder: (object: INearEarthObject) => void }> = ({object, className, isLunar = false, isOrdered, addOrder}) => {
-  const {id, name, is_potentially_hazardous_asteroid, close_approach_data, estimated_diameter: {meters: { estimated_diameter_max }}} = object;
+export const Asteroid:FunctionComponent<{isInOrder?: boolean; className?: string, object: INearEarthObject, isLunar?: boolean, isOrdered?: boolean, addOrder: (object: INearEarthObject) => void }> = ({object, className, isLunar = true, isOrdered, addOrder, isInOrder}) => {
+  const {name, is_potentially_hazardous_asteroid, close_approach_data, estimated_diameter: {meters: { estimated_diameter_max }}} = object;
   const {close_approach_date, miss_distance: {lunar, kilometers}} = close_approach_data[0];
 
   const shortName = useMemo(() => name.match(/\((.*)\)/)?.[1] ?? name, [name]);
@@ -14,7 +13,9 @@ export const Asteroid:FunctionComponent<{className?: string, object: INearEarthO
   return <div className={Asteroid__.Root}>
     <div className={Asteroid__.Date}>{close_approach_date}</div>
     <div className={Asteroid__.Row}>
-      <div className={Asteroid__.Distance}><span>{Math.round(Number(kilometers))} км</span></div>
+      <div className={Asteroid__.Distance}><span>
+        { isLunar ? `${Math.round(Number(lunar))} лунных орбит` : `${Math.round(Number(kilometers))} км`}
+        </span></div>
       <div className={Asteroid__.Image}></div>
       <div className={Asteroid__.Other}>
         <div className={Asteroid__.Name}>
@@ -26,7 +27,9 @@ export const Asteroid:FunctionComponent<{className?: string, object: INearEarthO
       </div>
     </div>
     <div className={Asteroid__.Footer}>
-      {isOrdered ? <div>В корзине</div> : <button type="button" onClick={onOrderClick}>Заказать</button>}
+        {!isInOrder &&
+      (isOrdered ? <div>В корзине</div> : <button type="button" onClick={onOrderClick}>Заказать</button>)}
+      {is_potentially_hazardous_asteroid && <div>Опасен</div>}
     </div>
   </div>
 }
